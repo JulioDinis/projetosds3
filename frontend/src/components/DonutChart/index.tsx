83ea5e7856918d298720f7a0e594830afa3a0637
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-11-08 02:23:56 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -16,22 +16,13 @@ function DonutChart() {
   })
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+    axios.get(`${BASE_URL}/salers/amount-by-seller`).then((response) => {
       const data = response.data as SaleSum[]
       const myLabels = data.map((x) => x.sellerName)
       const mySeries = data.map((x) => x.sum)
       setChartData({ labels: myLabels, series: mySeries })
     })
   }, [])
-  // forma errada
-  // let chartData: ChartData = { labels: [], series: [] }
-
-  // Forma Errada (chama mais de uma vez)
-
-  /*  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  } */
 
   const options = {
     legend: {
