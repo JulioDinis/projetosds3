16be8af066fe05f6c9549b49861083b6a5f36f91
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-11-08 02:59:09 -0300

    Alteração de 'salers' para 'sales' na API e em suas chamadas no frontend

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -31,7 +31,7 @@ const BarChart = () => {
   })
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/salers/success-by-seller`).then((response) => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
       const data = response.data as SaleSuccess[]
       const myLabels = data.map((x) => x.sellerName)
       const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1))
